78237298fa1725ca46f3bec6d725d624a2286404
noahcarrier1999 <car20111@byui.edu>
2024-06-20 07:59:31 -0600

    did the learning activity

---
+++ week02/analyze/MysteryProgram.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+
+class D
+{
+    static void Main()
+    {
+        int[] d = R(5);
+        Array.Sort(d);
+        Console.WriteLine("Values: " + string.Join(", ", d));
+        int s = C(d);
+        Console.WriteLine("Total: " + s);
+    }
+
+    static int[] R(int n)
+    {
+        //initializes Random Number
+        Random r = new Random();
+        //Makes a new list with the length of n
+        int[] d = new int[n];
+        //loops through list and puts a gets a random number between 1 and 7
+        for (int i = 0; i < n; i++)
+        {
+            d[i] = r.Next(1, 7);
+        }
+        //returns the list
+        return d;
+    }
+
+    static int C(int[] d)
+    {
+        int s = 0;
+        //Creates a new dictionary
+        Dictionary<int, int> c = new Dictionary<int, int>();
+        //loops through each of the ints in the d list
+        foreach (int x in d)
+        {
+            if (c.ContainsKey(x))
+            {
+                c[x]++;
+            }
+            else
+            {
+                c[x] = 1;
+            }
+        }
+        foreach (int v in c.Values)
+        {
+            switch (v)
+            {
+                case 2:
+                    s += 10;
+                    break;
+                case 3:
+                    s += 20;
+                    break;
+                case 4:
+                    s += 30;
+                    break;
+                case 5:
+                    s += 40;
+                    break;
+            }
+        }
+        return s;
+    }
+}
--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: 
 
         Console.WriteLine("------------");
 
@@ -23,12 +23,12 @@
         queue.Enqueue(300);
         queue.Enqueue(400);
         value = queue.Dequeue();
-        Console.WriteLine(value);
+        Console.WriteLine(value); //200
         value = queue.Dequeue();
-        Console.WriteLine(value);
+        Console.WriteLine(value);//300
         value = queue.Dequeue();
-        Console.WriteLine(value);
-        // Defect(s) Found: 
+        Console.WriteLine(value);//400
+        // Defect(s) Found: it should be index 0 not 1
 
         Console.WriteLine("------------");
 
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
